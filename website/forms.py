df5da649a963d9d1394955801935bf9e86746b4b
Sliverwall <Joediroberto@gmail.com>
2023-10-06 18:57:05 -0400

    Testing text on homepage

--- website/forms.py
+++ website/forms.py
@@ -37,15 +37,33 @@ class SignUpForm(UserCreationForm):
 
 # Create Add Patient Form
 class AddPatientForm(forms.ModelForm):
-	first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"First Name", "class":"form-control"}), label="")
-	last_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Last Name", "class":"form-control"}), label="")
-	DOB = forms.DateField(required=True, widget=forms.widgets.DateInput(attrs={"placeholder":"Date of Birth", "class":"form-control"}),label="Year-Month-Day")	
-	email = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Email", "class":"form-control"}), label="")
-	phone = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Phone", "class":"form-control"}), label="")
-	address = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Address", "class":"form-control"}), label="")
-	city = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"City", "class":"form-control"}), label="")
-	state = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"State", "class":"form-control"}), label="")
-	zipcode = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Zipcode", "class":"form-control"}), label="")
+	first_name = forms.CharField(required=True, 
+							  widget=forms.widgets.TextInput(attrs={"placeholder":"First Name", "class":"form-control"}), 
+							  label="")
+	last_name = forms.CharField(required=True, 
+							 widget=forms.widgets.TextInput(attrs={"placeholder":"Last Name", "class":"form-control"}), 
+							 label="")
+	DOB = forms.DateField(required=True, 
+					   widget=forms.widgets.DateInput(attrs={"placeholder":"Date of Birth", "class":"form-control"}),
+					   label="Year-Month-Day")	
+	email = forms.CharField(required=True, 
+						 widget=forms.widgets.TextInput(attrs={"placeholder":"Email", "class":"form-control"}), 
+						 label="")
+	phone = forms.CharField(required=True, 
+						 widget=forms.widgets.TextInput(attrs={"placeholder":"Phone", "class":"form-control"}), 
+						 label="")
+	address = forms.CharField(required=True, 
+						   widget=forms.widgets.TextInput(attrs={"placeholder":"Address", "class":"form-control"}), 
+						   label="")
+	city = forms.CharField(required=True, 
+						widget=forms.widgets.TextInput(attrs={"placeholder":"City", "class":"form-control"}), 
+						label="")
+	state = forms.CharField(required=True, 
+						 widget=forms.widgets.TextInput(attrs={"placeholder":"State", "class":"form-control"}),
+						   label="")
+	zipcode = forms.CharField(required=True, 
+						   widget=forms.widgets.TextInput(attrs={"placeholder":"Zipcode", "class":"form-control"}), 
+						   label="")
 
 	class Meta:
 		model = Patient
--- website/models.py
+++ website/models.py
@@ -10,7 +10,7 @@ class Patient(models.Model):
 	phone = models.CharField(max_length=15)
 	address =  models.CharField(max_length=100)
 	city =  models.CharField(max_length=50)
-	state =  models.CharField(max_length=50)
+	state =  models.CharField(max_length=2)
 	zipcode =  models.CharField(max_length=20)
 
 	def __str__(self):
