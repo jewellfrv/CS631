9cc25da2610106ee60c6a67e922cb4ab4a7d3eae
Jewell Vormawor <jewellvormawor@gmail.com>
2023-11-10 18:10:23 -0500

    ERD

--- NewarkMedicalAssociates/settings.py
+++ NewarkMedicalAssociates/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+GRAPH_MODELS = {
+  'all_applications': True,
+  'group_models': True,
+}
